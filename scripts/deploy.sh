cd698ef36e07046aac86b13d02f10768cd958f9c
Okema1 <fraserfall@gmail.com>
2023-01-06 15:45:47 -0500

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+touch lib/central_web/views/admin/general_view.ex
+
+if [ ! -f config/prod.secret.exs]
+then
+	cp documents/prod_files/example_prod_secret.exs config/prod.secret.exs
+fi
+
+sh scripts/build_container.sh
+sh scripts/generate_release.sh
+
+scp -i ~/.ssh/id_rsa rel/artifacts/teiserver.tar.gz deploy@yourdomain.com:/releases/teiserver.tar.gz
+
+mix phx.digest.clean --all
+
+echo "ssh into your server and run dodeploy"
